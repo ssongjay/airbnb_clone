c30734862d8bf27552777de8a20416344eed6ff1
ssongjay <jay5326@naver.com>
2023-06-13 17:03:58 +0900

    init: 백엔드 초기세팅

---
+++ airbnb_backend/src/main/java/com/example/airbnbclone/AirbnbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.airbnbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneApplication.class, args);
+	}
+
+}
---
+++ airbnb_backend/src/test/java/com/example/airbnbclone/AirbnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airbnbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
