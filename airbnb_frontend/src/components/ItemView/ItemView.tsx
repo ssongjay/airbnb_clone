6d8b48c43d99af64a9c642662b434a14f2a4e51d
Yeri Kim <rlafl7942@naver.com>
2023-06-29 18:03:55 +0900

    feat: 캐로셀 화면 레이아웃 구성

---
+++ airbnb_frontend/src/components/ItemView/ItemView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../ItemView/ItemView.scss";
+
+export default function ItemView() {
+  return (
+    <div className="itemViewContainer">
+      <div>알랄라</div>
+      안녕
+    </div>
+  );
+}
